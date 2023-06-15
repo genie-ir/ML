e7e9f2f98e7bffd7c856b48ad7e1df03129fe8a8
alihejrati <alihejrati1996@gmail.com>
2023-06-15 19:04:30 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -6,13 +6,6 @@ from utils.pl.plModuleBase import plModuleBase
 from libs.basicIO import signal_save, compressor
 
 class FUM(plModuleBase):
-    
-    
     def generator_step(self, batch):
         print('generator step')
         return None, {'loss': -1}
-    
-class D(nnModuleBase):
-    def forward(self, **kargs):
-        kargs['x'] = (kargs['x'] - .5) / .5
-        return super().forward(**kargs)
