f1a0645bb66cf85d8cf1a6a8d3d417e6374daab1
alihejrati <alihejrati1996@gmail.com>
2023-06-16 14:13:03 +0330

    .

--- utils/pl/plModuleBase.py
+++ utils/pl/plModuleBase.py
@@ -359,7 +359,7 @@ class plModuleBase(pl.LightningModule):
     #     assert False, 'END'
 
     def get_pretrained_model(self, config=None, model=None, freezeFlag=True, keys=None):
-        keys = str('' if keys is None else keys)
+        keys = str('' if keys is None else keys).split('.')
         print('---->', keys)
         if isinstance(config, str):
             config = instantiate_from_config({'target': config}, kwargs={'dotdictFlag': False})
