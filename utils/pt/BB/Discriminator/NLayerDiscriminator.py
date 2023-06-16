06f45b07443a7717034c667af7b60f2d618c780e
alihejrati <alihejrati1996@gmail.com>
2023-06-16 19:20:41 +0330

    .

---
+++ utils/pt/BB/Discriminator/NLayerDiscriminator.py
@@ -0,0 +1,66 @@
+import torch
+import functools
+from torch import nn
+from utils.pt.building_block import BB
+from utils.pt.BB.Norm.ActNorm import ActNorm
+
+class NLayerDiscriminator(BB):
+    """Defines a PatchGAN discriminator as in Pix2Pix
+        --> see https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
+    """
+    def start(self):
+        """Construct a PatchGAN discriminator
+        Parameters:
+            input_nc (int)  -- the number of channels in input images
+            ndf (int)       -- the number of filters in the last conv layer
+            n_layers (int)  -- the number of conv layers in the discriminator
+            norm_layer      -- normalization layer
+        """
+        input_nc=self.kwargs.get('input_nc', 3)
+        ndf=self.kwargs.get('ndf', 64)
+        n_layers=self.kwargs.get('n_layers', 3)
+        use_actnorm=self.kwargs.get('use_actnorm', False)
+
+        print(input_nc, ndf, n_layers, use_actnorm)
+        assert False
+
+        if not use_actnorm:
+            norm_layer = nn.BatchNorm2d
+        else:
+            norm_layer = ActNorm
+        if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
+            use_bias = norm_layer.func != nn.BatchNorm2d
+        else:
+            use_bias = norm_layer != nn.BatchNorm2d
+
+        kw = 4
+        padw = 1
+        sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
+        nf_mult = 1
+        nf_mult_prev = 1
+        for n in range(1, n_layers):  # gradually increase the number of filters
+            nf_mult_prev = nf_mult
+            nf_mult = min(2 ** n, 8)
+            sequence += [
+                nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=use_bias),
+                norm_layer(ndf * nf_mult),
+                nn.LeakyReLU(0.2, True)
+            ]
+
+        nf_mult_prev = nf_mult
+        nf_mult = min(2 ** n_layers, 8)
+        sequence += [
+            nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias),
+            norm_layer(ndf * nf_mult),
+            nn.LeakyReLU(0.2, True)
+        ]
+
+        sequence += [
+            nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
+        self.main = nn.Sequential(*sequence)
+
+    def forward(self, input):
+        """Standard forward."""
+        input = torch.cat([input[:,0:1 ,:,:], input[:,1:2 ,:,:], input[:,3:4 ,:,:]], dim=1)
+        # logger.critical(input.shape)
+        return self.main(input) # I think 3x256x256 -> 1x30x30
--- utils/pt/building_block.py
+++ utils/pt/building_block.py
@@ -22,11 +22,21 @@ class BB(nn.Module):
 
         self.kwargs = kwargs_new
         self.start()
+        if bool(self.kwargs.get('apply_weights_init', True)): # TODO
+            self.apply(getattr(self, self.kwargs.get('weights_init_fn_name', 'weights_init')))
     
     def start(self):
         cowsay.cow('NotImplementedError:\nplease define `{}` function for `{}` building block.'.format('start', self.__class__.__name__))
         sys.exit()
     
+    def weights_init(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1 and hasattr(m, 'weight'):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1 and hasattr(m, 'weight') and hasattr(m, 'bias'):
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
+
     def visualize(self, **kwargs):
         # print('----------->', id(self), kwargs)
         # kwargs['create_node'](id(self))
