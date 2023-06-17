4807bfcc4fbbe2ce866c25c1ea18016cf24ba1b8
alihejrati <alihejrati1996@gmail.com>
2023-06-17 17:22:56 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -10,6 +10,9 @@ class FUM(plModuleBase):
     def generator_step(self, batch):
         y = batch['y']
         xf0 = batch[self.signal_key]
+        print(xf0[xf0>0].shape)
+        print(xf0[xf0<0].shape)
+        assert False
         xfN = torch.randn(xf0.shape, device=self.device) / 600
         print(xf0.min().item(), xf0.max().item())
         print(xfN.min().item(), xfN.max().item())
