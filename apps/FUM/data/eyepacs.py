a931bcbd12c7629349e70a53ce016292e60382f5
alihejrati <alihejrati1996@gmail.com>
2023-06-17 13:30:02 +0330

    .

--- apps/FUM/data/eyepacs.py
+++ apps/FUM/data/eyepacs.py
@@ -3,11 +3,12 @@ import numpy as np
 from os.path import join, exists
 from utils.pt.datasets.D import D_Base
 from os import system, getenv, makedirs
+from utils.analysis.fourier.basic import x2fr
 from data.config.eyepacs.D import eyepacsTrain as eyepacsTrainBase, eyepacsValidation as eyepacsValidationBase
 
 class D(D_Base):
     def fetch(self, signal_path):
-        signal = np.load(signal_path).astype(np.float32)
+        signal = np.reshape(np.load(signal_path), (1, -1)).astype(np.float32)
         print(signal.shape)
         assert False
         return {
