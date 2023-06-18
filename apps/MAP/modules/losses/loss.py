746c91807c034db6af03b55b9aa2a9f51f766247
alihejrati <alihejrati1996@gmail.com>
2023-06-19 00:58:48 +0330

    .

--- apps/MAP/modules/losses/loss.py
+++ apps/MAP/modules/losses/loss.py
@@ -6,5 +6,8 @@ class Loss(LossBase):
 class OHLoss(LossBase):
     def lossfn(self, Yi, Yip1, yi, yip1):
         print('*******************', Yi.shape, Yip1.shape, yi.shape, yip1.shape)
-    
-    pass
+        li = self.criterion(yi, Yi)
+        lip1 = self.criterion(yip1, Yip1)
+
+        print(li, lip1)
+        assert False
