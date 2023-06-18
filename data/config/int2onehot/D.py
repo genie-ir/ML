ea7068448ccfc2893c4c7de756aaa0de1baf8464
alihejrati <alihejrati1996@gmail.com>
2023-06-18 19:26:48 +0330

    .

--- data/config/int2onehot/D.py
+++ data/config/int2onehot/D.py
@@ -4,18 +4,22 @@ from utils.pt.datasets.D import D_Base
 class D(D_Base):
     def start(self):
         self.M = int(self.kwargs.get('M', 1))
-        self._length = int(self.kwargs.get('N', -1)) - 1
+        self.N = int(self.kwargs.get('N', -1))
+        self.oh = np.eye(self.N)
+        self._length = self.N - 1
 
     def __getitem__(self, i):
         Xi = np.random.uniform(0, .5, (self.M, 1)) + i
         Xip1 = np.random.uniform(.5, 1, (self.M, 1)) + i + 1
 
-        print(Xi)
+        print(Xi, self.oh[i])
         print('-'*30)
-        print(Xip1)
+        print(Xip1, self.oh[i+1])
         assert False
 
         return {
             'Xi': Xi,
-            'Xip1': Xip1
+            'Xip1': Xip1,
+            'Yi': self.oh[i],
+            'Yi+1': self.oh[i+1]
         }
