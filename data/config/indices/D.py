d760f7eef6c32e3c62a0d284d50c7278486eaf14
alihejrati <alihejrati1996@gmail.com>
2023-06-18 18:59:36 +0330

    .

--- data/config/indices/D.py
+++ data/config/indices/D.py
@@ -4,15 +4,18 @@ from utils.pt.datasets.D import D_Base
 class D(D_Base):
     def start(self):
         self.M = int(self.kwargs.get('M', 1))
-        self._length = int(self.kwargs.get('N', -1))
+        self._length = int(self.kwargs.get('N', -1)) - 1
 
     def __getitem__(self, i):
-        X = np.zeros((self.M, 1)) + i
+        Xi = np.random.uniform(0, .5, (self.M, 1)) + i
+        Xip1 = np.random.uniform(.5, 1, (self.M, 1)) + i + 1
 
-        print(X)
+        print(Xi)
+        print('-'*30)
+        print(Xip1)
         assert False
 
         return {
-            'X': i,
-            'Y': 0
+            'Xi': Xi,
+            'Xip1': Xip1
         }
