528f04be6167046392e88684b5053d80d01d108d
alihejrati <alihejrati1996@gmail.com>
2023-06-19 16:01:39 +0330

    .

--- apps/MAP/modules/losses/loss.py
+++ apps/MAP/modules/losses/loss.py
@@ -7,7 +7,6 @@ class OHLoss(LossBase):
     def lossfn(self, Yi, Yip1, yi, yip1):
         loss1, loss2 = self.criterion(yi, Yi) , self.criterion(yip1, Yip1)
         loss = loss1 + loss2
-        print(loss1.item(), loss2.item())
         log = {
             'loss': loss.clone().detach().mean(),
         }
