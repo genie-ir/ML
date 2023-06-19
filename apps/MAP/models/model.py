027fa166f79d11471abaf3af813021b5f4b83745
alihejrati <alihejrati1996@gmail.com>
2023-06-19 15:56:14 +0330

    .

--- apps/MAP/models/model.py
+++ apps/MAP/models/model.py
@@ -6,18 +6,6 @@ from utils.pl.plModuleBase import plModuleBase
 from libs.basicIO import signal_save, compressor
 
 class MAP(plModuleBase):
-    def validation_step(self, batch, batch_idx, split='val'):
-        print(
-            'val --->', self.OH.net_seq0[0].weight[0,0]
-        )
-        return super().validation_step(batch, batch_idx, split)
-    
-    def training_step(self, batch, batch_idx, split='train'):
-        print(
-            'train --->', self.OH.net_seq0[0].weight[0,0]
-        )
-        return super().training_step(batch, batch_idx, split)
-    
     def OH_step(self, batch):
         return self.OHLoss(
             batch['Yi'].long(), 
