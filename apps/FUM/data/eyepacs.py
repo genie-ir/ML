02934f1087f0b3f64fa06fae51935788b624daab
alihejrati <alihejrati1996@gmail.com>
2023-06-20 22:23:02 +0330

    .

--- apps/FUM/data/eyepacs.py
+++ apps/FUM/data/eyepacs.py
@@ -8,20 +8,20 @@ from utils.analysis.fourier.basic import x2fr, fr2x_torch
 from data.config.eyepacs.D import eyepacsTrain as eyepacsTrainBase, eyepacsValidation as eyepacsValidationBase
 
 # N0, N1, B0 = 1, 1, 0
-N0, N1, B0 = 600, 512, 50
-M0, M1 = 100, 50
+# N0, N1, B0 = 600, 512, 50
+# M0, M1 = 100, 50
 
-def normalizing(signal: np.ndarray):
-    signal = x2fr(np.reshape(signal, (1, -1))).squeeze() / N1
-    return ((((signal + B0) / N0) * M0) - M1).astype(np.float32)
+# def normalizing(signal: np.ndarray):
+#     signal = x2fr(np.reshape(signal, (1, -1))).squeeze() / N1
+#     return ((((signal + B0) / N0) * M0) - M1).astype(np.float32)
 
-def denormalizing(signal: torch.tensor):
-    return fr2x_torch((((signal + M1) / M0) * N0 - B0) * N1) 
+# def denormalizing(signal: torch.tensor):
+#     return fr2x_torch((((signal + M1) / M0) * N0 - B0) * N1) 
 
 class D(D_Base):
     def fetch(self, signal_path):
         return {
-            'latentcode': normalizing(np.load(signal_path))
+            'latentcode': np.load(signal_path)
         }
 
 class eyepacsTrain(eyepacsTrainBase): 
--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -8,18 +8,15 @@ from apps.FUM.data.eyepacs import denormalizing
 from utils.pt.tricks.error_grade import error_grade
 
 class FUM(plModuleBase):
-    def start(self):
-        self.counter = -1
-
     def validation_step(self, batch, batch_idx, split='val'):
         pass
 
     def generator_step(self, batch):
         y = batch['y']
-        xf = error_grade(batch[self.signal_key], 3)
+        # xf = error_grade(batch[self.signal_key], 3)
         # xf = self.generator(x=xf)
-
-        xt = denormalizing(xf)
+        # xt = denormalizing(xf)
+        xt = batch[self.signal_key]
         phi = self.vqgan.rec_phi({'x': xt, 'y': y})
         self.vqgan.save_phi(phi, pathdir='/content')
 
