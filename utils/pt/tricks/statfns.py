6328683f006c641d04356122d1d3618c581f5df5
alihejrati <alihejrati1996@gmail.com>
2023-06-21 22:43:38 +0330

    .

--- utils/pt/tricks/statfns.py
+++ utils/pt/tricks/statfns.py
@@ -9,9 +9,7 @@ def correlation(x, y=None, kill_itself=False):
             return torch.corrcoef(x) * (1-torch.eye(x.shape[0])).detach()
     
     # Not diffrantiable (currently)
-    x = x.detach().numpy()
-    y = y.detach().numpy()
     out = []
     for i in range(x.shape[0]):
-        out.append(torch.corrcoef(torch.cat([x[i:i+1, :], y], dim=0))[0, :])
+        out.append(torch.corrcoef(torch.cat([x[i:i+1, :], y], dim=0))[0, :].detach().numpy())
     return torch.tensor(np.array(out))
