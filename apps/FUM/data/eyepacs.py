fd6820fd2e30c8ecba0347904c248231abb06e0e
alihejrati <alihejrati1996@gmail.com>
2023-06-21 18:30:07 +0330

    .

--- apps/FUM/data/eyepacs.py
+++ apps/FUM/data/eyepacs.py
@@ -27,7 +27,7 @@ class D(D_Base):
         for k in self.init_clusters:
             n = torch.tensor(np.corrcoef(self.init_clusters[k]))
             m = torch.topk(L2S_VQ(n, n), 2, largest=False)
-            print(k, m, m.shape)
+            print(k, m, type(m))
             assert False
 
         # print('all unique', self.all_unique_init_clusters, len(self.all_unique_init_clusters))
