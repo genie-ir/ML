884602be092bae4385036587ea764583e5d08eca
alihejrati <alihejrati1996@gmail.com>
2023-07-16 19:04:06 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -79,7 +79,7 @@ class FUM(plModuleBase):
     
     def start(self):
         print('!!!!!!!', self.ncluster, self.embed_size)
-        self.scodebook = VectorQuantizer(n_e=self.ncluster, e_dim=self.embed_size, beta=0.25, zch=1)
+        self.scodebook = VectorQuantizer(n_e=self.ncluster, e_dim=1, beta=0.25, zch=1)
         # self.ccodebook = VectorQuantizer(n_e=(self.ncrosses * self.ncluster), e_dim=self.embed_size, beta=0.25, zch=1)
     
     # def generator_step00(self, batch):
