cb58274603096a2b38e1784d2e5b222883c7c77b
alihejrati <alihejrati1996@gmail.com>
2023-07-18 14:00:44 +0330

    .

--- data/config/range/dataset.py
+++ data/config/range/dataset.py
@@ -3,6 +3,8 @@ from utils.pt.datasets.D import D_Base
 class D(D_Base):
     def start(self):
         self.set_length(self.config.get('range', 1e3))
+        print('#######################', self.__length)
+        assert False
     
     def __getitem__(self, i):
         return {
