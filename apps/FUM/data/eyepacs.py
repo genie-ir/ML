d37767bf40fb765fd11f79ed020820fdb785461b
alihejrati <alihejrati1996@gmail.com>
2023-07-19 18:21:47 +0330

    .

--- apps/FUM/data/eyepacs.py
+++ apps/FUM/data/eyepacs.py
@@ -1,93 +1,76 @@
-# from data.config.range.D import D as Drange
+import yaml
+import torch
+import numpy as np
+from libs.basicIO import dfread
+from os.path import join, exists
+from utils.pt.datasets.D import D_Base
+from data.config.eyepacs.D import eyepacsTrain as eyepacsTrainBase, eyepacsValidation as eyepacsValidationBase
 
-# class D(Drange):
-#     pass
+class D(D_Base):
+    # def start(self):
+    #     self.set_length(5e3)
 
-
-
-
-
-
-# import yaml
-# import torch
-# import numpy as np
-# from libs.basicIO import dfread
-# from os.path import join, exists
-# from utils.pt.datasets.D import D_Base
-# from os import system, getenv, makedirs
-# from data.config.eyepacs.D import eyepacsTrain as eyepacsTrainBase, eyepacsValidation as eyepacsValidationBase
-
-# from utils.pt.distance import L2S
-# from utils.np.statfns import correlation
-# from utils.pt.BB.KDE.kde import plot_kde
-
-
-
-# class D(D_Base):
-#     # def start(self):
-#     #     self.set_length(5e3)
-
-#     def __start(self):
-#         self._length = 10
-#         return
-#         STATIC_PATH = join('1DsignalOfEyepacs', self.kwargs['DATASET_CATEGORY'], 'Grade_')
-#         print('------>', STATIC_PATH)
-#         self._length = 0
-#         df_candidate = dfread(self.kwargs['DF_CANDIDATE_PATH'])
-#         self.init_clusters = dict()
-#         for dfc_dr in df_candidate.dr.unique():
-#             df_candidate_dr = df_candidate[df_candidate.dr == dfc_dr]
-#             self.init_clusters['class_' + str(dfc_dr)] = np.array([np.load(join(
-#                 self.kwargs['UPPER_PATH'],
-#                 STATIC_PATH + str(dfc_dr),
-#                 df_candidate_dr.iloc[dfc_idx].image_id)).flatten().astype(np.float32) for dfc_idx in range(len(df_candidate_dr))])
-#         self.all_unique_init_clusters = np.unique(np.array([self.init_clusters[k] for k in self.init_clusters])) 
+    def __start(self):
+        self._length = 10
+        return
+        STATIC_PATH = join('1DsignalOfEyepacs', self.kwargs['DATASET_CATEGORY'], 'Grade_')
+        print('------>', STATIC_PATH)
+        self._length = 0
+        df_candidate = dfread(self.kwargs['DF_CANDIDATE_PATH'])
+        self.init_clusters = dict()
+        for dfc_dr in df_candidate.dr.unique():
+            df_candidate_dr = df_candidate[df_candidate.dr == dfc_dr]
+            self.init_clusters['class_' + str(dfc_dr)] = np.array([np.load(join(
+                self.kwargs['UPPER_PATH'],
+                STATIC_PATH + str(dfc_dr),
+                df_candidate_dr.iloc[dfc_idx].image_id)).flatten().astype(np.float32) for dfc_idx in range(len(df_candidate_dr))])
+        self.all_unique_init_clusters = np.unique(np.array([self.init_clusters[k] for k in self.init_clusters])) 
         
 
 
 
 
 
-#         # for k in self.init_clusters:
-#             # n = torch.tensor(self.init_clusters[k]) / (30*1024)
-#             # plot_kde(D=n, h=1e1, r=.001, s=.01, path='/content/KDE/{}.png'.format(k))
-#             # assert False
+        # for k in self.init_clusters:
+            # n = torch.tensor(self.init_clusters[k]) / (30*1024)
+            # plot_kde(D=n, h=1e1, r=.001, s=.01, path='/content/KDE/{}.png'.format(k))
+            # assert False
 
-#             # m = n.unique()
-#             # print(k, m.shape)
-#             # m = n.corrcoef()
-#             # m = np.abs(correlation(n.detach().numpy(), kill_itself=True))
-#             # m = torch.topk(L2S(n, n), 2, largest=False).values[:, 1]
-#             # M = m[m<1e-2]
-#             # M = M[M>0]
-#             # print(k, m.min(), m.max(), m.dtype, m.shape)
+            # m = n.unique()
+            # print(k, m.shape)
+            # m = n.corrcoef()
+            # m = np.abs(correlation(n.detach().numpy(), kill_itself=True))
+            # m = torch.topk(L2S(n, n), 2, largest=False).values[:, 1]
+            # M = m[m<1e-2]
+            # M = M[M>0]
+            # print(k, m.min(), m.max(), m.dtype, m.shape)
             
-#             # for kj in self.init_clusters:
-#             #     if k == kj:
-#             #         continue
-#             #     _n = torch.tensor(self.init_clusters[kj])
-#             #     m = np.abs(correlation(n.detach().numpy(), _n.detach().numpy()))
-#             #     print('class {} -> {}'.format(k, kj), m.min(), m.max(), m.dtype, m.shape)
-#             # print()
+            # for kj in self.init_clusters:
+            #     if k == kj:
+            #         continue
+            #     _n = torch.tensor(self.init_clusters[kj])
+            #     m = np.abs(correlation(n.detach().numpy(), _n.detach().numpy()))
+            #     print('class {} -> {}'.format(k, kj), m.min(), m.max(), m.dtype, m.shape)
+            # print()
 
-#         # print('all unique', self.all_unique_init_clusters, len(self.all_unique_init_clusters))
-#         assert False
+        # print('all unique', self.all_unique_init_clusters, len(self.all_unique_init_clusters))
+        assert False
 
-#     # def __getitem__(self, i):
-#     #     return {
-#     #         'latentcode': i
-#     #     }
+    # def __getitem__(self, i):
+    #     return {
+    #         'latentcode': i
+    #     }
     
-#     def fetch(self, signal_path):
-#         return {
-#             'latentcode': np.load(signal_path)
-#             # 'noisecode': 
-#         }
+    def fetch(self, signal_path):
+        return {
+            'latentcode': np.load(signal_path)
+            # 'noisecode': 
+        }
 
-# class eyepacsTrain(eyepacsTrainBase): 
-#     def preparation(self, **kwargs):
-#         self.D = D
+class eyepacsTrain(eyepacsTrainBase): 
+    def preparation(self, **kwargs):
+        self.D = D
     
-# class eyepacsValidation(eyepacsValidationBase):
-#     def preparation(self, **kwargs):
-#         self.D = D
+class eyepacsValidation(eyepacsValidationBase):
+    def preparation(self, **kwargs):
+        self.D = D
