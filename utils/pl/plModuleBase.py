2bc813ce69503c31c6c2cbe18e5baff8d5dcba3f
alihejrati <alihejrati1996@gmail.com>
2023-07-29 21:22:57 +0330

    .

--- utils/pl/plModuleBase.py
+++ utils/pl/plModuleBase.py
@@ -311,7 +311,7 @@ class plModuleBase(pl.LightningModule):
             for opt in optimizers_list:
                 # print('------------>', opt)
                 opt.zero_grad()
-            print('!!!!!!!!!!!!!!!!', loss.shape, loss.reuires_grad)
+            print('!!!!!!!!!!!!!!!!', loss.shape, loss.requires_grad)
             self.manual_backward(loss)
             optimizers_list[optimizer_idx].step()
         
