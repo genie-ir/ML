5afb32e2a9f501b722d3e5c4fb551b94ab5f8dbe
alihejrati <alihejrati1996@gmail.com>
2023-07-31 01:40:55 +0330

    .

--- data/config/random/dataset.py
+++ data/config/random/dataset.py
@@ -7,24 +7,24 @@ class D(D_Base):
         self.length = int(self.config.get('length', 1e3))
         self.key = str(self.config.get('key', 'indices'))
         self.getitemfn = str(self.config.get('getitemfn', 'identity'))
-        setattr(self, '__getitem__', getattr(self, '__getitem__' + self.getitemfn))
+        setattr(self, '__getitem__', getattr(self, self.getitemfn))
         self.set_length(self.length)
 
         if self.getitemfn == 'randint':
             self.low = int(self.config.get('low', 0))
             self.high = int(self.config.get('high', None))
     
-    def __getitem__identity(self, i):
+    def identity(self, i):
         return {
             self.key: i
         }
     
-    def __getitem__randint(self, i):
+    def randint(self, i):
         return {
             self.key: np.random.randint(self.low, self.high, self.shape)
         }
     
-    def __getitem__randn(self, i):
+    def randn(self, i):
         return {
             self.key: np.random.normal(size=self.shape)
         }
