b5658c1ad8686f340b60789f5fd20cf2a2bedca5
alihejrati <alihejrati1996@gmail.com>
2023-08-02 01:56:26 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -41,7 +41,7 @@ class FUM(plModuleBase):
             self.sethooks(self.generator.ccodebook.embedding.weight, hooks=lambda grad: print('w $$$$$$$$$$$$$$$$$$$$$$$$$$', grad.shape, grad[2, :3], grad[t, :3], grad[11, :3]))
             self.sethooks(batch[self.signal_key], hooks=[
                 lambda grad: grad * 10**C,
-                lambda grad: print('x $$$$$$$$$$$$$$$$$$$$$$$$$$', grad.shape, grad[:, :3])
+                # lambda grad: print('x $$$$$$$$$$$$$$$$$$$$$$$$$$', grad.shape, grad[:, :3])
             ])
             # print(f'B{batch_idx}', batch[self.signal_key].shape, batch[self.signal_key].dtype, batch[self.signal_key].requires_grad)
             # batch[self.signal_key].requires_grad_(True)
