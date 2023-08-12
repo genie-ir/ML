66c7f21e8f2e3b7afb43713d52333a41c3682167
alihejrati <alihejrati1996@gmail.com>
2023-08-12 19:29:02 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -91,10 +91,6 @@ class FUM(plModuleBase):
         # scphi = self.vqgan.qua2phi(self.generator.mac[C](sq))
 
         
-        
-        # dloss_phi = phi.mean()
-
-
         dloss_phi = -torch.mean(self.vqgan.loss.discriminator(phi))
         # dloss_scphi = -torch.mean(self.vqgan.loss.discriminator(scphi))
         loss_phi = self.lambda_loss_phi * self.LeakyReLU(dloss_phi - self.gamma)
