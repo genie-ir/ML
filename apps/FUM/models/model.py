dafdda8625a39fdc4ecdda31d29f9dd5d23e8ca9
alihejrati <alihejrati1996@gmail.com>
2023-08-28 11:11:28 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -5,8 +5,7 @@ from utils.pt.BB.Calculation.residual_block import MAC
 from torchmetrics.functional.image import structural_similarity_index_measure as SSIM
 from utils.pt.BB.Quantizer.VectorQuantizer import VectorQuantizer as VectorQuantizerBase
 
-#  TODO: this is a test 
-
+# TODO we looking for uniqness.
 class VectorQuantizer(VectorQuantizerBase):
     def embedding_weight_init(self):
         self.w = self.nnParameter(tensor=torch.randint(0, 1024, self.eshape).log())
@@ -53,32 +52,37 @@ class FUM(plModuleBase):
             MAC(units=2, shape=self.qshape) for c in range(self.nclasses)
         ])
 
-    def __c2phi(self, c, batch_size):
-        # phi = self.vqgan.lat2phi(c)
+    def __c2phi(self, cross, batch_size):
+        # BASIC the very basic code of idea behind chaining concept.
+        # phi = self.vqgan.lat2phi(cross)
         # sn = self.vqgan.phi2lat(phi).flatten(1).float().detach()
-        # return phi, sn 
+        # return phi, sn
         
-        latent = c
-        old_rec_metric = -1
-        s1 = torch.zeros((batch_size,) + self.phi_shape, device=self.device)
-        # s2 = torch.zeros(phi_shape, device=self.device)
-        for N in range(1, self.phi_steps + 1):
-            phi = self.vqgan.lat2phi(latent)
-            s1 = s1 + phi
-            latent_rec = self.vqgan.phi2lat(phi).float()
-            rec_metric = ((latent-latent_rec) ** 2).mean()
+        old_quantization_error = float('inf')
+        phi0 = self.vqgan.lat2phi(cross)
+        # latent = self.vqgan.phi2lat(phi0).float().detach()
+        phi = phi0.detach()
+        # IDEA s1 = phi0 # is better than --> torch.zeros((batch_size,) + self.phi_shape, device=self.device)   becuse the first one is diffrentiable.
+        for N in range(1, self.phi_steps):
+            # s1 = s1 + phi
+            latent_index = self.vqgan.phi2lat(phi).float()
+            phi_of_latent_index = self.vqgan.lat2phi(latent_index)
             print('latent', latent.shape, latent.dtype)
-            print('latent_rec', latent_rec.shape, latent_rec.dtype)
-            print('--rec_metric-->', rec_metric)
-            latent = latent_rec
-            self.vqgan.save_phi(phi, pathdir=self.pathdir, fname=f'phi-{str(N)}.png')
-            if rec_metric < 1e-6 or old_rec_metric == rec_metric:
+            print('latent_index', latent_index.shape, latent_index.dtype)
+            quantization_error = ((phi-phi_of_latent_index) ** 2).mean()
+            print('---quantization_error-->', quantization_error)
+            latent = latent_index
+            phi = phi_of_latent_index
+            # self.vqgan.save_phi(phi, pathdir=self.pathdir, fname=f'phi-{str(N)}.png')
+            print(f'{N}--- old_quantization_error - quantization_error --->', (old_quantization_error - quantization_error).item(), (old_quantization_error - quantization_error) < 1e-6)
+            if quantization_error < 1e-6 or old_quantization_error == quantization_error:
                 break
-            old_rec_metric = rec_metric
+            old_quantization_error = quantization_error
+            
         # compressor(self.pathdir, self.pathdir + '/phi.zip')
-        mue = (s1 / N).detach()
+        # mue = (s1 / N).detach()
         sn = latent.detach()
-        return mue, sn
+        return phi0, sn
     
     def generator_step(self, batch):
         bidx = batch['bidx']
@@ -89,7 +93,7 @@ class FUM(plModuleBase):
         # print('ln', ln.shape, ln.dtype)
         # print('phi', phi.shape, phi.dtype)
         # print('sn', sn.shape, sn.dtype)
-        # print('SN', SN.shape, SN.dtype, SN) #NOTE: nearset latents to sn
+        # print('SN', SN.shape, SN.dtype, SN) #NOTE: index of nearset latents to sn
         
         
         
@@ -121,7 +125,6 @@ class FUM(plModuleBase):
             # drloss_scphi=drloss_scphi,
             Class=torch.tensor(float(cidx))
         )
-
         print(f'cidx={cidx}', lossdict)
         # for i in range(5):
         #     for j in range(5):
@@ -135,145 +138,3 @@ class FUM(plModuleBase):
         # self.vqgan.save_phi(scphi, pathdir=self.pathdir, fname=f'final/{bidx}-{cidx}/scphi.png')
 
         return loss, lossdict
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class _FUM(plModuleBase):
-    def resnet50(self, model):
-        model.fc = nn.Linear(model.fc.in_features, 1)
-        return model
-
-    def validation_step(self, batch, batch_idx, split='val'):
-        pass
-    
-    def __start(self):
-        self.seqlen = 3 # 256
-        self.seqdim = 2 # 1
-        self.vocab_size = 451
-        self.transformer = Transformer(
-            heads=getattr(self, 'heads', 1),
-            maxlen=getattr(self, 'maxlen', 1e3),
-            dropout=getattr(self, 'dropout', 0),
-            fwd_expan=getattr(self, 'fwd_expan', 4),
-            num_layers=getattr(self, 'num_layers', 8),
-            trg_mask=getattr(self, 'trg_mask', True),
-            src_mask=getattr(self, 'src_mask', False),
-            embed_size=getattr(self, 'latent_dim', 256),
-            trg_vocab_size=getattr(self, 'trg_vocab_size', 1e3),
-            src_vocab_size=getattr(self, 'src_vocab_size', 1e3)
-        )
-
-    def generator_step(self, batch):
-        # latent = self.ccodebook(batch[self.signal_key])[0].view(-1, self.qwh, self.qwh)
-        latent = batch[self.signal_key].float()
-        old_rec_metric = -1
-        phi_shape = (batch['batch_size'], self.phi_ch, self.phi_wh, self.phi_wh)
-        s1 = torch.zeros(phi_shape, device=self.device)
-        # s2 = torch.zeros(phi_shape, device=self.device)
-        for N in range(1, self.phi_steps + 1):
-            phi = self.vqgan.lat2phi(latent)
-            s1 = s1 + phi
-            break
-            # s2 = s2 + phi ** 2
-            latent_rec = self.vqgan.phi2lat(phi).float()
-            rec_metric = (latent-latent_rec).abs().sum()
-            # print('--lm-->', rec_metric)
-            latent = latent_rec
-            # self.vqgan.save_phi(phi, pathdir=self.pathdir, fname=f'phi-{str(N)}.png')
-            if rec_metric < 1e-6 or old_rec_metric == rec_metric:
-                break
-            old_rec_metric = rec_metric
-        # compressor(self.pathdir, self.pathdir + '/phi.zip')
-        phi = s1 / N
-        # print('!!!!!!!!!!!!! mue', mue.shape, mue.dtype, mue.requires_grad)
-        p = self.vqgan.phi2lat(phi).float().flatten(1).unsqueeze(-1).unsqueeze(-1)
-        # print('!!!!!!!!!!!!!! p', p.shape, p.dtype, p.requires_grad)
-        s, sloss = self.scodebook(p)
-        # print('!!!!!!!!!!!!!! s', s.shape, s.dtype, s.requires_grad)
-        # sq = self.qw * self.vqgan.lat2qua(s) + self.qb
-        # sq = self.vqgan.lat2qua(s)
-        # print('++++++++++++++>', batch['y'])
-        self.vqgan.save_phi(phi, pathdir=self.pathdir, fname=f'final/phi-{str(N)}.png')
-        
-        dloss_phi = -torch.mean(self.vqgan.loss.discriminator(phi))
-        loss_phi = self.LeakyReLU(dloss_phi - self.gamma)
-        loss = self.lambda_loss_phi * loss_phi 
-        ld = dict()
-        for c in range(self.nclasses):
-            scphic = self.vqgan.qua2phi(self.mac[c](self.vqgan.lat2qua(s)))
-            print('-------------->', c)
-            # print('-------------->', self.drclassifire(scphic))
-            self.vqgan.save_phi(scphic, pathdir=self.pathdir, fname=f'final/scphic({c})-{str(N)}.png')
-            dloss_scphic = -torch.mean(self.vqgan.loss.discriminator(scphic))
-            loss_scphic = self.lambda_loss_scphic[c] * self.LeakyReLU(dloss_scphic - self.gamma)
-            drloss_scphic = self.lambda_drloss_scphic[c] * torch.tensor(1, device=self.device) #* self.drclassifire(scphic).mean()
-            ld[f'loss_scphic_{c}'] = loss_scphic.clone().detach().mean()
-            ld[f'drloss_scphic_{c}'] = drloss_scphic.clone().detach().mean()
-            loss = loss + loss_scphic + drloss_scphic
-
-        lossdict = self.generatorLoss._lossdict(
-            loss=loss,
-            loss_phi=loss_phi,
-            dloss_phi=dloss_phi,
-            **ld
-        )
-
-        print('@@@@@@@@@@@@@@@', lossdict)
-
-        assert False
-        return loss, lossdict
-    
-    def start(self):
-        if not isinstance(self.lambda_loss_scphic, (list, tuple)):
-            lambda_loss_scphic = float(self.lambda_loss_scphic)
-            self.lambda_loss_scphic = [lambda_loss_scphic for c in range(self.nclasses)]
-        
-        if not isinstance(self.lambda_drloss_scphic, (list, tuple)):
-            lambda_drloss_scphic = float(self.lambda_drloss_scphic)
-            self.lambda_drloss_scphic = [lambda_drloss_scphic for c in range(self.nclasses)]
-
-        self.qshape = (self.qch, self.qwh, self.qwh)
-        self.mac = nn.Sequential(*[
-            MAC(units=2, shape=self.qshape) for c in range(self.nclasses)
-        ])
-        # self.qw = nn.Parameter(torch.randn(self.qshape))
-        # self.qb = nn.Parameter(torch.randn(self.qshape))
-        self.scodebook = VectorQuantizer(n_e=self.ncluster, e_dim=self.latent_dim, beta=0.25, zwh=1)
-        self.ccodebook = VectorQuantizer(n_e=(self.ncrosses * self.ncluster), e_dim=self.latent_dim, beta=0.25, zwh=1)
-        self.LeakyReLU = torch.nn.LeakyReLU(negative_slope=self.negative_slope, inplace=False)
-
-    # def generator_step00(self, batch):
-    #     x = self.codebook(batch[self.signal_key])
-    #     phi = self.vqgan.rec_phi({'x': x, 'y': batch['y']})
-    #     self.vqgan.save_phi(phi, pathdir='/content')
-
-    #     g_loss = -torch.mean(self.vqgan.loss.discriminator(phi.contiguous()))
-    #     print('g_loss', g_loss.shape, g_loss, g_loss.requires_grad)
-    #     assert False
-    #     return g_loss, {'loss': g_loss.item()}
---
+++ apps/FUM/models/model_v0_backup.py
@@ -0,0 +1,116 @@
+# DELETE this file shoude be delete later it is just for backuping of main code.
+
+
+class _FUM(plModuleBase):
+    def resnet50(self, model):
+        model.fc = nn.Linear(model.fc.in_features, 1)
+        return model
+
+    def validation_step(self, batch, batch_idx, split='val'):
+        pass
+    
+    def __start(self):
+        self.seqlen = 3 # 256
+        self.seqdim = 2 # 1
+        self.vocab_size = 451
+        self.transformer = Transformer(
+            heads=getattr(self, 'heads', 1),
+            maxlen=getattr(self, 'maxlen', 1e3),
+            dropout=getattr(self, 'dropout', 0),
+            fwd_expan=getattr(self, 'fwd_expan', 4),
+            num_layers=getattr(self, 'num_layers', 8),
+            trg_mask=getattr(self, 'trg_mask', True),
+            src_mask=getattr(self, 'src_mask', False),
+            embed_size=getattr(self, 'latent_dim', 256),
+            trg_vocab_size=getattr(self, 'trg_vocab_size', 1e3),
+            src_vocab_size=getattr(self, 'src_vocab_size', 1e3)
+        )
+
+    def generator_step(self, batch):
+        # latent = self.ccodebook(batch[self.signal_key])[0].view(-1, self.qwh, self.qwh)
+        latent = batch[self.signal_key].float()
+        old_rec_metric = -1
+        phi_shape = (batch['batch_size'], self.phi_ch, self.phi_wh, self.phi_wh)
+        s1 = torch.zeros(phi_shape, device=self.device)
+        # s2 = torch.zeros(phi_shape, device=self.device)
+        for N in range(1, self.phi_steps + 1):
+            phi = self.vqgan.lat2phi(latent)
+            s1 = s1 + phi
+            break
+            # s2 = s2 + phi ** 2
+            latent_rec = self.vqgan.phi2lat(phi).float()
+            rec_metric = (latent-latent_rec).abs().sum()
+            # print('--lm-->', rec_metric)
+            latent = latent_rec
+            # self.vqgan.save_phi(phi, pathdir=self.pathdir, fname=f'phi-{str(N)}.png')
+            if rec_metric < 1e-6 or old_rec_metric == rec_metric:
+                break
+            old_rec_metric = rec_metric
+        # compressor(self.pathdir, self.pathdir + '/phi.zip')
+        phi = s1 / N
+        # print('!!!!!!!!!!!!! mue', mue.shape, mue.dtype, mue.requires_grad)
+        p = self.vqgan.phi2lat(phi).float().flatten(1).unsqueeze(-1).unsqueeze(-1)
+        # print('!!!!!!!!!!!!!! p', p.shape, p.dtype, p.requires_grad)
+        s, sloss = self.scodebook(p)
+        # print('!!!!!!!!!!!!!! s', s.shape, s.dtype, s.requires_grad)
+        # sq = self.qw * self.vqgan.lat2qua(s) + self.qb
+        # sq = self.vqgan.lat2qua(s)
+        # print('++++++++++++++>', batch['y'])
+        self.vqgan.save_phi(phi, pathdir=self.pathdir, fname=f'final/phi-{str(N)}.png')
+        
+        dloss_phi = -torch.mean(self.vqgan.loss.discriminator(phi))
+        loss_phi = self.LeakyReLU(dloss_phi - self.gamma)
+        loss = self.lambda_loss_phi * loss_phi 
+        ld = dict()
+        for c in range(self.nclasses):
+            scphic = self.vqgan.qua2phi(self.mac[c](self.vqgan.lat2qua(s)))
+            print('-------------->', c)
+            # print('-------------->', self.drclassifire(scphic))
+            self.vqgan.save_phi(scphic, pathdir=self.pathdir, fname=f'final/scphic({c})-{str(N)}.png')
+            dloss_scphic = -torch.mean(self.vqgan.loss.discriminator(scphic))
+            loss_scphic = self.lambda_loss_scphic[c] * self.LeakyReLU(dloss_scphic - self.gamma)
+            drloss_scphic = self.lambda_drloss_scphic[c] * torch.tensor(1, device=self.device) #* self.drclassifire(scphic).mean()
+            ld[f'loss_scphic_{c}'] = loss_scphic.clone().detach().mean()
+            ld[f'drloss_scphic_{c}'] = drloss_scphic.clone().detach().mean()
+            loss = loss + loss_scphic + drloss_scphic
+
+        lossdict = self.generatorLoss._lossdict(
+            loss=loss,
+            loss_phi=loss_phi,
+            dloss_phi=dloss_phi,
+            **ld
+        )
+
+        print('@@@@@@@@@@@@@@@', lossdict)
+
+        assert False
+        return loss, lossdict
+    
+    def start(self):
+        if not isinstance(self.lambda_loss_scphic, (list, tuple)):
+            lambda_loss_scphic = float(self.lambda_loss_scphic)
+            self.lambda_loss_scphic = [lambda_loss_scphic for c in range(self.nclasses)]
+        
+        if not isinstance(self.lambda_drloss_scphic, (list, tuple)):
+            lambda_drloss_scphic = float(self.lambda_drloss_scphic)
+            self.lambda_drloss_scphic = [lambda_drloss_scphic for c in range(self.nclasses)]
+
+        self.qshape = (self.qch, self.qwh, self.qwh)
+        self.mac = nn.Sequential(*[
+            MAC(units=2, shape=self.qshape) for c in range(self.nclasses)
+        ])
+        # self.qw = nn.Parameter(torch.randn(self.qshape))
+        # self.qb = nn.Parameter(torch.randn(self.qshape))
+        self.scodebook = VectorQuantizer(n_e=self.ncluster, e_dim=self.latent_dim, beta=0.25, zwh=1)
+        self.ccodebook = VectorQuantizer(n_e=(self.ncrosses * self.ncluster), e_dim=self.latent_dim, beta=0.25, zwh=1)
+        self.LeakyReLU = torch.nn.LeakyReLU(negative_slope=self.negative_slope, inplace=False)
+
+    # def generator_step00(self, batch):
+    #     x = self.codebook(batch[self.signal_key])
+    #     phi = self.vqgan.rec_phi({'x': x, 'y': batch['y']})
+    #     self.vqgan.save_phi(phi, pathdir='/content')
+
+    #     g_loss = -torch.mean(self.vqgan.loss.discriminator(phi.contiguous()))
+    #     print('g_loss', g_loss.shape, g_loss, g_loss.requires_grad)
+    #     assert False
+    #     return g_loss, {'loss': g_loss.item()}
