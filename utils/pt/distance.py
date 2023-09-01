b97569ca7fafbb9dd555975405d88a02a134c21c
alihejrati <alihejrati1996@gmail.com>
2023-09-01 22:31:46 +0330

    .

--- utils/pt/distance.py
+++ utils/pt/distance.py
@@ -11,12 +11,16 @@ from einops import rearrange
 #             + torch.sum(b**2, dim=1) \
 #             -2 * torch.matmul(a, b.t())
 
-def L2S(a, b, argmin=False):
+def L2S(a, b, argmin=False, topk=1):
     """Square of L2 Norm"""
     d = torch.sum(a ** 2, dim=1, keepdim=True) + \
         torch.sum(b ** 2, dim=1) - 2 * \
         torch.einsum('bd,dn->bn', a, rearrange(b, 'n d -> d n'))
     
     if argmin:
-        return torch.argmin(d, dim=1)
+        amin = torch.argmin(d, dim=1)
+        tk = torch.topk(d, topk, largest=False, dim=1)
+        print('amin', amin)
+        print('tk', tk)
+        assert False
     return d
