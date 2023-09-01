678d067c748dc3dc2f2be1e2752e38a9bd654b47
alihejrati <alihejrati1996@gmail.com>
2023-09-01 22:36:29 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -60,6 +60,7 @@ class FUM(plModuleBase):
         ])
 
     def __c2phi(self, cross, tag=''):
+        print('hooooooooooooooooo!!')
         nearst_cross = self.generator.scodebook.fwd_nbpi(self.generator.scodebook.fwd_getIndices(cross.unsqueeze(-1).unsqueeze(-1)).squeeze()).exp().detach()
         # list_of_distance_to_mode = []
         # BASIC the very basic code of idea behind chaining concept.
