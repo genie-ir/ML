8ed27e4690fbf5525ec40aaea3e7a17401bff415
alihejrati <alihejrati1996@gmail.com>
2023-09-02 00:44:27 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -31,7 +31,8 @@ class FUM(plModuleBase):
 
         # B = batch[self.signal_key]
         t = 3612
-        B = torch.tensor([2,2,2,2, t], device=self.device)
+        # B = torch.tensor([2,2,2,2, t], device=self.device)
+        B = torch.tensor([2], device=self.device)
         b = B[0]
         print(f'iter{batch_idx} | before', self.generator.scodebook.embedding.weight[b,0], self.generator.scodebook.embedding.weight[b,0].exp())
         for cidx in range(self.nclasses):
