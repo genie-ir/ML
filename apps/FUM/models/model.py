45ae904fadabc227395a707505b054dada9d5f96
alihejrati <alihejrati1996@gmail.com>
2023-09-08 20:03:13 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -25,7 +25,7 @@ class FUM(plModuleBase):
         return
     
     def training_step(self, batch, batch_idx, split='train'):
-        self.vqgan.save_phi(batch, pathdir=self.pathdir, fname=f'batch.png')
+        self.vqgan.save_phi(batch[self.signal_key], pathdir=self.pathdir, fname=f'batch.png')
         assert False
 
     def training_step0(self, batch, batch_idx, split='train'):
