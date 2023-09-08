9e517b8397002d4960271d04acaa9dbe917ea73f
alihejrati <alihejrati1996@gmail.com>
2023-09-09 00:23:17 +0330

    .

--- libs/basicIO.py
+++ libs/basicIO.py
@@ -335,7 +335,10 @@ def __signal_save__img_Tensor(images, fpath, nrow=None, fn=None, sreturn=False):
         if isinstance(images, torch.Tensor):
             images = images.detach().cpu()
         
-        print(images.shape)
+        print(images.shape, images.min(), images.max())
+        imgs = fn(images)
+        print(imgs.shape, imgs.min(), imgs.max())
+
         assert False
         grid = torchvision.utils.make_grid(images, nrow=nrow) # this grid finally contains table of iamges like this -> [images[k].shape[0]/nrow, nrow] ; Notic: grid is tensor with shape: ch x h? x w?
         grid = fn(grid).numpy().astype(np.uint8)
