6b780119c3ebe69043605c5995797de073391656
alihejrati <alihejrati1996@gmail.com>
2023-09-13 15:39:43 +0330

    .

--- dependency/MKCNet/dataset/dataset.py
+++ dependency/MKCNet/dataset/dataset.py
@@ -34,7 +34,7 @@ class basic_dataset(Dataset):
 
 
                 
-                signal_save(T, f'/content/dataset/{scn}.png', stype='img', chw2hwc=True)
+                signal_save(T, f'/content/dataset/{scn}.png', stype='img', sparams={'chw2hwc': True})
 
 
 
