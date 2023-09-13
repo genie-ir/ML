f61932d3deab81ecb0ef5abe5abe01fa76c04e6b
alihejrati <alihejrati1996@gmail.com>
2023-09-13 16:05:38 +0330

    .

--- dependency/MKCNet/dataset/dataset_manager.py
+++ dependency/MKCNet/dataset/dataset_manager.py
@@ -56,11 +56,11 @@ def get_transform(cfg):
     transfrom_train.append(A.SmallestMaxSize(256))
     transfrom_test.append(A.SmallestMaxSize(256))
 
-    transfrom_train.append(ToTensorV2())
     transfrom_train.append(A.Normalize(means, std))
+    transfrom_train.append(ToTensorV2())
 
-    transfrom_test.append(ToTensorV2())
     transfrom_test.append(A.Normalize(means, std))
+    transfrom_test.append(ToTensorV2())
 
     train_ts =  A.Compose(transfrom_train)
     test_ts = A.Compose(transfrom_test)
