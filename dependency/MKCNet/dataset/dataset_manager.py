bf51cce2a9fbf9972dd5ce68737ffde8d3bda070
alihejrati <alihejrati1996@gmail.com>
2023-09-13 13:23:12 +0330

    .

--- dependency/MKCNet/dataset/dataset_manager.py
+++ dependency/MKCNet/dataset/dataset_manager.py
@@ -3,7 +3,7 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 
 def get_dataloader(cfg):
-    print('@@@@@@@@', cfg.DATASET.DATADIR)
+    print('@@@@@@@@', cfg.DATASET.get('DATADIR'))
     assert False
     root = cfg.DATASET.ROOT
     batch_size = cfg.BATCH_SIZE
