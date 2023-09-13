c36a79d2655db77add18a2c317206fa81f5bc662
alihejrati <alihejrati1996@gmail.com>
2023-09-13 14:20:37 +0330

    .

--- dependency/MKCNet/dataset/dataset.py
+++ dependency/MKCNet/dataset/dataset.py
@@ -20,7 +20,8 @@ class basic_dataset(Dataset):
             next(f)
             for line in f:
                 line = self._modifyline_(line, dataset_name) # modify the label of DEEPDR and EYEQ
-                self.data.append(self._readimage_(osp.join(self.mapsplit[split], line[0].split('_')[0], line[0]), dataset_name))
+                fs, sc = line[0].split('/')
+                self.data.append(self._readimage_(osp.join(self.mapsplit[split], fs, sc.split('_')[0], sc), dataset_name))
                 self.label_T.append(int(line[1]))
                 self.label_IQ.append(int(line[2]))
                 self.label_M.append(int(line[2]) * num_T + int(line[1]))
