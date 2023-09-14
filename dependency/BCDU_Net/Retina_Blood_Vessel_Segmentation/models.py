279b5db7ac972d087050c508d8a1d6de0b00db7d
alihejrati <alihejrati1996@gmail.com>
2023-09-14 15:35:01 +0330

    .

--- dependency/BCDU_Net/Retina_Blood_Vessel_Segmentation/models.py
+++ dependency/BCDU_Net/Retina_Blood_Vessel_Segmentation/models.py
@@ -14,6 +14,7 @@ import numpy as np
 def BCDU_net_D3(input_size = (256,256,1)):
     N = input_size[0]
     inputs = Input(input_size) 
+    assert False, '1111111111111'
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
   
