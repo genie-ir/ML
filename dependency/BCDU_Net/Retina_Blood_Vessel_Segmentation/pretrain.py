25c5880c58ae3c9d051914d4e09c6b8bf8a313a9
alihejrati <alihejrati1996@gmail.com>
2023-09-15 10:25:29 +0330

    .

--- dependency/BCDU_Net/Retina_Blood_Vessel_Segmentation/pretrain.py
+++ dependency/BCDU_Net/Retina_Blood_Vessel_Segmentation/pretrain.py
@@ -20,7 +20,7 @@ def pretrain(ckpt):
         # print('D @@@@@@@@@@@', img.shape, img.dtype)
         img = rearrange(img, 'b c h w -> b h w c')
         # print('E @@@@@@@@@@@', img.shape, img.dtype)
-        predictions = model.predict(img, batch_size=2, verbose=1)
+        predictions = model.predict(img, batch_size=2, verbose=0)
         # print('F @@@@@@@@@@@', predictions.shape, predictions.dtype)
         predictions = rearrange(predictions, 'b h w c -> b c h w')
         # print('G @@@@@@@@@@@', predictions.shape, predictions.dtype)
--- dependency/MKCNet/dataset/dataset.py
+++ dependency/MKCNet/dataset/dataset.py
@@ -14,15 +14,21 @@ import matplotlib.pyplot as plt
 from mlxtend.plotting import plot_confusion_matrix
 from libs.basicIO import compressor
 
+def np_default_print():
+    np.set_printoptions(edgeitems=3, infstr='inf', linewidth=75, nanstr='nan', precision=8,
+        suppress=False, threshold=1000, formatter=None)
+
 def cmatrix(Y_TRUE, Y_PRED, path, normalize=False):
     conf_matrix = confusion_matrix(y_true=Y_TRUE, y_pred=Y_PRED)
     if normalize:
-        conf_matrix = conf_matrix / conf_matrix.astype(np.float32).sum(axis=1)
+        conf_matrix = conf_matrix.astype('float') / conf_matrix.sum(axis=1)[:, np.newaxis]
+    np.set_printoptions(precision=2, suppress=True, formatter={'float': '{:0.2f}%'.format})
     fig, ax = plot_confusion_matrix(conf_mat=conf_matrix, figsize=(6, 6), cmap=plt.cm.Greens)
     plt.xlabel('Predictions', fontsize=18)
     plt.ylabel('Actuals', fontsize=18)
     plt.title('Confusion Matrix', fontsize=18)
     fig.savefig(path, dpi=1200)
+    np_default_print()
 
 
 
