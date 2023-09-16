532ffe8db53bae1d62759c54ba8951bbdc35f9a5
alihejrati <alihejrati1996@gmail.com>
2023-09-16 20:16:55 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -22,6 +22,43 @@ except Exception as e:
 
 
 import tensorflow.keras
+import torch
+import models_vit
+from util.pos_embed import interpolate_pos_embed
+from timm.models.layers import trunc_normal_
+
+
+def getdrmodel():
+    # call the model
+    model = models_vit.__dict__['vit_large_patch16'](
+        num_classes=2,
+        drop_path_rate=0.2,
+        global_pool=True,
+    )
+
+    # load RETFound weights
+    ckpt = '/content/drive/MyDrive/storage/dependency/RETFound_cfp_weights.pth'
+    checkpoint = torch.load(ckpt, map_location='cpu')
+    checkpoint_model = checkpoint['model']
+    state_dict = model.state_dict()
+    for k in ['head.weight', 'head.bias']:
+        if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
+            print(f"Removing key {k} from pretrained checkpoint")
+            del checkpoint_model[k]
+
+    # interpolate position embedding
+    interpolate_pos_embed(model, checkpoint_model)
+
+    # load pre-trained model
+    msg = model.load_state_dict(checkpoint_model, strict=False)
+
+    assert set(msg.missing_keys) == {'head.weight', 'head.bias', 'fc_norm.weight', 'fc_norm.bias'}
+
+    # manually initialize fc layer
+    trunc_normal_(model.head.weight, std=2e-5)
+
+    print("Model = %s" % str(model))
+    return model
 
 
 # TODO we looking for uniqness.
@@ -152,7 +189,7 @@ class FUM(plModuleBase):
         from libs.basicIO import pathBIO
         from dependency.MKCNet.dataset.dataset_manager import get_dataloader
         from dependency.BCDU_Net.Retina_Blood_Vessel_Segmentation.pretrain import pretrain as makevaslsegmentation
-        self.drc = tensorflow.keras.models.load_model('/content/ML/dependency/drclassifire_django/keras_model.h5')
+        self.drc = getdrmodel().to('cuda')
         self.vseg = makevaslsegmentation('/content/drive/MyDrive/storage/dr_classifire/unet-segmentation/weight_retina.hdf5')
         # cfg = makeDRclassifire('/content/drive/MyDrive/storage/dr_classifire/best_model.pth')
         tasknet, cfg = makeDRclassifire('/content/drive/MyDrive/storage/dr_classifire/best_model.pth')
@@ -162,7 +199,7 @@ class FUM(plModuleBase):
         train_loader, test_loader, val_loader, dataset_size = get_dataloader(cfg, 
             vqgan=self.vqgan,
             # tasknet=self.tasknet,
-            drc = self.drc,
+            drc=self.drc,
             vseg=self.vseg
         )
         print('done')
