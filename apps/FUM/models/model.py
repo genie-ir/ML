0576a481cf15076cf7f8a05499fc267a64882cc4
alihejrati <alihejrati1996@gmail.com>
2023-09-19 17:54:03 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -209,10 +209,10 @@ class FUM(plModuleBase):
         from dependency.MKCNet.dataset.dataset_manager import get_dataloader
         from dependency.BCDU_Net.Retina_Blood_Vessel_Segmentation.pretrain import pretrain as makevaslsegmentation
         # self.vseg = makevaslsegmentation('/content/drive/MyDrive/storage/dr_classifire/unet-segmentation/weight_retina.hdf5')
-        # self.tasknet, cfg = makeDRclassifire('/content/drive/MyDrive/storage/dr_classifire/best_model.pth')
-        # self.tasknet = self.tasknet.to('cuda')
-        # self.generator.tasknet = self.tasknet
-        # self.tasknet.requires_grad_(False)
+        self.dr_classifire, cfg = makeDRclassifire('/content/drive/MyDrive/storage/dr_classifire/best_model.pth')
+        self.dr_classifire = self.dr_classifire.to('cuda')
+        self.generator.dr_classifire = self.dr_classifire
+        # self.dr_classifire.requires_grad_(False)
         # self.vseg = makevaslsegmentation('/content/drive/MyDrive/storage/dr_classifire/unet-segmentation/weight_retina.hdf5')
         # self.train_ds, self.test_ds, self.val_ds, dataset_size = get_dataloader(cfg, 
         #     # vqgan=self.vqgan,
