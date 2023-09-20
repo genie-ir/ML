0c69772aa66c9dab731b7aeb4e083a0d2371b17a
alihejrati <alihejrati1996@gmail.com>
2023-09-20 19:40:20 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -95,6 +95,7 @@ class FUM(plModuleBase):
         phi = self.vqgan.lat2phi(batch['X'].flatten(1).float())
         phi_denormalized = self.vqgan_fn_phi_denormalize(phi).detach()
         print('-------------------->', phi.shape, phi_denormalized.shape)
+
         phi_denormalized = dzq_dz_eq1(phi_denormalized, phi)
         # signal_save(phi_denormalized, f'/content/gstep/{random_string()}.png', stype='img', sparams={'chw2hwc': True})
         # dr_pred = self.softmax(self.dr_classifire(phi_denormalized)[0])
@@ -248,7 +249,7 @@ class FUM(plModuleBase):
 
     def start(self):
 
-        self.vqgan_fn_phi_denormalize = lambda G: ((((G.clamp(-1., 1.))+1)/2)*255).transpose(0,1).transpose(1,2)
+        self.vqgan_fn_phi_denormalize = lambda G: ((((G.clamp(-1., 1.))+1)/2)*255)#.transpose(0,1).transpose(1,2)
         self.ce = nn.CrossEntropyLoss()
         self.softmax = torch.nn.Softmax(dim=1)
         self.t_ypred = []
