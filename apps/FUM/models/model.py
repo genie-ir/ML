97cbab00fb8e71e7cc805a541f8d1cc7a1f89445
alihejrati <alihejrati1996@gmail.com>
2023-09-22 15:19:23 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -88,10 +88,7 @@ class FUM(plModuleBase):
             self.v_ypred = self.v_ypred + list(dr_pred.argmax(dim=1).cpu().numpy())
             self.v_ygrnt = self.v_ygrnt + list(batch['y_edit'].cpu().numpy())
 
-        A = loss.cpu().detach().item()
-        print(A, type(A))
-        assert False
-        return loss, {'loss', loss.cpu().detach().item()}
+        return loss, dict(loss=loss.cpu().detach().item())
 
     
     # NOTE: Synthesis Algorithm.
