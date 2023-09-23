3a96a6f2d87f5fc8cc23c393fe78e1695a433965
alihejrati <alihejrati1996@gmail.com>
2023-09-23 10:41:18 +0330

    .

--- apps/VQGAN/data/utils.py
+++ apps/VQGAN/data/utils.py
@@ -154,8 +154,10 @@ def custom_collate(batch):
         return torch.tensor(batch, dtype=torch.float64)
     elif isinstance(elem, int):
         return torch.tensor(batch)
-    elif isinstance(elem, string_classes):
+    elif isinstance(elem, str):
         return batch
+    # elif isinstance(elem, string_classes):
+    #     return batch
     elif isinstance(elem, collections.abc.Mapping):
         return {key: custom_collate([d[key] for d in batch]) for key in elem}
     elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
