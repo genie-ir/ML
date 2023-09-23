fa3ccded9d216c6084607c0754007ad5d3520350
alihejrati <alihejrati1996@gmail.com>
2023-09-23 19:49:28 +0330

    .

--- apps/VQGAN/modules/dataset.py
+++ apps/VQGAN/modules/dataset.py
@@ -16,8 +16,8 @@ class WrappedDataset(WrappedDatasetBase):
 class DataModuleFromConfig(DataModuleFromConfigBase):
     def __init__(self, **kwargs):
         # kwargs['wrap']=True # this line it shoulde be remove. TODO
-        os.system('kaggle datasets download -d andrewmvd/drive-digital-retinal-images-for-vessel-extraction')
-
+        # os.system('kaggle datasets download -d andrewmvd/drive-digital-retinal-images-for-vessel-extraction')
+        extractor('/content/drive2004.zip', '/content/dataset_drive')
         kwargs['custom_collate'] = custom_collate
         kwargs['instantiate_from_config'] = Config.instantiate_from_config
         super().__init__(**kwargs)
