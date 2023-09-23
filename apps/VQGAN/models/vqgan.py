3d3f8b311906cb31f28a5d57ca502d8751e4c675
alihejrati <alihejrati1996@gmail.com>
2023-09-23 10:50:23 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -178,7 +178,8 @@ class VQModel(pl.LightningModule):
         print('validation_step')
         x = self.get_input(batch, self.image_key)
         xrec, qloss = self(x)
-        vasl = None # self.get_input(batch, 'vasl')
+        # vasl = None # self.get_input(batch, 'vasl')
+        vasl = self.get_input(batch, 'vasl')
         aeloss, log_dict_ae = self.loss(qloss, x, xrec, 0, self.global_step, last_layer=self.get_last_layer(), split="val", cond=vasl)
         discloss, log_dict_disc = self.loss(qloss, x, xrec, 1, self.global_step, last_layer=self.get_last_layer(), split="val", cond=vasl)
         # rec_loss = log_dict_ae["val/rec_loss"]
