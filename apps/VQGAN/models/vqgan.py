c9f7a0f26362194bb5277bdab19e85d1d0a4d04b
alihejrati <alihejrati1996@gmail.com>
2023-09-23 20:10:22 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -210,6 +210,7 @@ class VQModel(pl.LightningModule):
         Tf = A.Compose([
             A.Resize(256, 256),
             A.CLAHE(clip_limit=4.0, tile_grid_size=(8, 8), always_apply=True, p=1.0),
+            ToGray(),
             ToTensorV2()
         ])
         Tm = A.Compose([
@@ -218,7 +219,7 @@ class VQModel(pl.LightningModule):
         ])
         fundus_drive = np.array(Image.open('/content/dataset_drive/DRIVE/training/images/24_training.tif'))
         fd = fundus_drive
-        fundus_drive = (fundus_drive[:, :,:,0:1] + fundus_drive[:, :,:,1:2] + fundus_drive[:, :,:,2:3]) / 3
+        # fundus_drive = (fundus_drive[:,:,0] + fundus_drive[:,:,1] + fundus_drive[:,:,2]) / 3
         fundus_drive = fundus_drive.astype(np.uint8)
         fundus_mask = np.array(Image.open('/content/dataset_drive/DRIVE/training/1st_manual/24_manual1.gif'))
         
