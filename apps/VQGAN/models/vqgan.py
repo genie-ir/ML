a6e013d8a71b0b85ed3078d855f805417e95f43f
alihejrati <alihejrati1996@gmail.com>
2023-09-23 18:44:09 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -197,7 +197,7 @@ class VQModel(pl.LightningModule):
         rr = self.vqgan_fn_phi_denormalize(logged['reconstructions'])
         for i in range(logged['inputs'].shape[0]):
             r = rr[i]
-            R.append(T(image=rearrange(r, 'c h w -> h w c').cpu().detach().numpy().astype(np.uint8))['image'].to('cuda'))
+            R.append(T(image=rearrange(r, 'c h w -> h w c').cpu().detach().numpy().astype(np.uint8))['image'].unsqueeze(0).to('cuda'))
         self.save_phi(torch.cat([
             logged['inputs'],
             torch.cat(R, dim=0)
