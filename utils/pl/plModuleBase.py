9ba0670bd533aa2245edf97aa7a4c04342de0d98
alihejrati <alihejrati1996@gmail.com>
2023-09-30 15:51:15 +0330

    .

--- utils/pl/plModuleBase.py
+++ utils/pl/plModuleBase.py
@@ -296,7 +296,9 @@ class plModuleBase(pl.LightningModule):
     
     def getbatch(self, batch):
         """It can be overwrite in child class"""
-        batch['batch_size'] = batch[self.signal_key].shape[0]
+        # batch['batch_size'] = batch[self.signal_key].shape[0]
+        batch['batch_size'] = len(batch['x'])
+        print('--------------------------------------------------->', batch['batch_size'])
         batch['device'] = self.device
         batch['Self'] = self
         return batch
