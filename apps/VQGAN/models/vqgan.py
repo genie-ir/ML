8fc35c34a0da6e2e3762fafe6f50f42f039dc2d1
alihejrati <alihejrati1996@gmail.com>
2023-10-08 13:05:12 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -279,7 +279,7 @@ class VQModel(pl.LightningModule):
         
         # aeloss, log_dict_ae = self.loss(qloss, x, xrec, 0, self.global_step, last_layer=self.get_last_layer(), split="train"
             # , cond=vasl
-        )
+        # )
         
         aeloss = torch.mean(torch.abs(x - xrec) + 0.1 * self.loss.perceptual_loss(x, xrec))
         VLOSS = 0.5 * torch.mean(torch.abs(Vorg - Vrec) + 0.1 * self.loss.perceptual_loss(Vorg, Vrec)).log()
