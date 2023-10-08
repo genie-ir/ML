2828aeee31a497d00f1b2e34ce3fcb9946cfd9d2
alihejrati <alihejrati1996@gmail.com>
2023-10-08 12:55:42 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -262,17 +262,21 @@ class VQModel(pl.LightningModule):
         #     signal_save(x, f'/content/.png', stype='img', sparams={'chw2hwc': True})
         # xrec, qloss = self(x)
         # return dec, qloss
-
+        Vorg = self.vseg((((x.cpu() +1)*127.5).detach())).detach()
         xrec, qloss = self.drcQ(x, w)
 
         xr = self.vqgan_fn_phi_denormalize(xrec).detach()
+        Vrec = self.vseg(xr.cpu()).detach()
+        Vrec = dzq_dz_eq1(Vrec, xrec)
         xr = dzq_dz_eq1(xr, xrec)
         xr = (xr - (self.dr_classifire_normalize_mean * 255)) / (self.dr_classifire_normalize_std * 255)
         drloss = CE(S(DRC(xr)[0].detach()), clabel)
 
 
-        Vorg, Vrec = self.get_V2(x, xr)
-        Vrec = dzq_dz_eq1(Vrec, xrec)
+        # Vorg, Vrec = self.get_V2(x, xr)
+        
+        
+        
         aeloss, log_dict_ae = self.loss(qloss, x, xrec, 0, self.global_step, last_layer=self.get_last_layer(), split="train"
             # , cond=vasl
         )
