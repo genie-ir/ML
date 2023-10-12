834525a6160f60dfb62002f8f6021d0d8e743520
alihejrati <alihejrati1996@gmail.com>
2023-10-12 11:21:50 +0330

    .

--- apps/FUM/modules/losses/loss.py
+++ apps/FUM/modules/losses/loss.py
@@ -1,5 +1,10 @@
-from utils.pt.lossBase import LossBase
-from utils.pt.losses.cgan import CGANLossBase
+try:
+    from utils.pt.lossBase import LossBase
+    from utils.pt.losses.cgan import CGANLossBase
+except Exception as e:
+    print(e)
+    assert False
+
 
 class Loss(LossBase):
     pass
