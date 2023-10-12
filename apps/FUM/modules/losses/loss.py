44cf7caacaf3849c86be40ec6d4d7cc7a1cc26cf
alihejrati <alihejrati1996@gmail.com>
2023-10-12 11:22:42 +0330

    .

--- apps/FUM/modules/losses/loss.py
+++ apps/FUM/modules/losses/loss.py
@@ -1,10 +1,11 @@
 try:
     from utils.pt.lossBase import LossBase
     from utils.pt.losses.cgan import CGANLossBase
+
+    class Loss(LossBase):
+        pass
 except Exception as e:
     print(e)
     assert False
 
 
-class Loss(LossBase):
-    pass
