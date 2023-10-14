addb637c2797db83a87960903345f86bf74201a3
alihejrati <alihejrati1996@gmail.com>
2023-10-15 01:48:21 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -97,13 +97,13 @@ class FUM(plModuleBase):
     
     def check_dr(self, y_edit, split, batch_index, dr_pred):
         # TODO uncomment it
-        dr_pred_s = self.generator.softmax(dr_pred)
-        if split == 'train':
-            self.t_ypred = self.t_ypred + list(dr_pred_s.argmax(dim=1).cpu().numpy())
-            self.t_ygrnt = self.t_ygrnt + list(y_edit.cpu().numpy())
-        else:
-            self.v_ypred = self.v_ypred + list(dr_pred_s.argmax(dim=1).cpu().numpy())
-            self.v_ygrnt = self.v_ygrnt + list(y_edit.cpu().numpy())
+        # dr_pred_s = self.generator.softmax(dr_pred)
+        # if split == 'train':
+        #     self.t_ypred = self.t_ypred + list(dr_pred_s.argmax(dim=1).cpu().numpy())
+        #     self.t_ygrnt = self.t_ygrnt + list(y_edit.cpu().numpy())
+        # else:
+        #     self.v_ypred = self.v_ypred + list(dr_pred_s.argmax(dim=1).cpu().numpy())
+        #     self.v_ygrnt = self.v_ygrnt + list(y_edit.cpu().numpy())
         
         
         loss = self.generator.ce(dr_pred, y_edit) # cross entropy doing softmax inide of own.
@@ -604,14 +604,14 @@ class FUM_DR(FUM):
     #     torch.set_grad_enabled(False)
     #     return super().validation_step(batch, batch_idx, split)
 
-    def on_train_epoch_end(self):
-        self.v_ygrnt = self.v_ygrnt + [1,2]
-        self.t_ygrnt = self.t_ygrnt + [1,2]
-        self.v_ypred = self.v_ypred + [1,2]
-        self.t_ypred = self.t_ypred + [1,2]
-        cmatrix(self.v_ygrnt, self.v_ypred, f'/content/e0_val_cmat_before.png', normalize=True)
-        cmatrix(self.t_ygrnt, self.t_ypred, f'/content/e0_train_cmat_before.png', normalize=True, title='before')
-        assert False, 'END-TRAINING'
+    # def on_train_epoch_end(self):
+    #     self.v_ygrnt = self.v_ygrnt + [1,2]
+    #     self.t_ygrnt = self.t_ygrnt + [1,2]
+    #     self.v_ypred = self.v_ypred + [1,2]
+    #     self.t_ypred = self.t_ypred + [1,2]
+    #     cmatrix(self.v_ygrnt, self.v_ypred, f'/content/e0_val_cmat_before.png', normalize=True)
+    #     cmatrix(self.t_ygrnt, self.t_ypred, f'/content/e0_train_cmat_before.png', normalize=True, title='before')
+    #     assert False, 'END-TRAINING'
 
     # def validation_step(self, batch, batch_idx, split='val'):
     #     return
