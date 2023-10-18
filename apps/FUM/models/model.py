36fe578e4d39659be04c2b73f33e359e1ef213df
alihejrati <alihejrati1996@gmail.com>
2023-10-18 05:57:07 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -124,7 +124,7 @@ class FUM(plModuleBase):
     def drc_master(self, batch, **kwargs):
         drpred = self.vgg16(
             batch['xs'] # normalized like this: xs = xs/127.5 - 1
-        )
+        ).reshape(-1, 1, 64, 64)
 
         # drpred = self.generator.vggout(self.vgg16(
         #     batch['xs'] # normalized like this: xs = xs/127.5 - 1
@@ -543,6 +543,7 @@ class FUM_DR(FUM):
     def start(self, dr_vs_synthesis_flag=True):
         super().start(dr_vs_synthesis_flag=False)
         
+        self.generator.c2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros', device=None, dtype=None)
         self.generator.vggout = nn.Sequential(
             nn.Linear(in_features=1000, out_features=300),
             nn.ReLU(inplace=True),
