05d0742a33573ff4c8914a221b575f24ad44a75f
alihejrati <alihejrati1996@gmail.com>
2023-10-22 01:50:30 +0330

    .

--- apps/FUM/models/model.py
+++ apps/FUM/models/model.py
@@ -653,12 +653,12 @@ class FUM_DR(FUM):
         self.t_ygrnt = self.t_ygrnt + [1,2]
         self.v_ypred = self.v_ypred + [1,2]
         self.t_ypred = self.t_ypred + [1,2]
-        cmatrix(self.v_ygrnt, self.v_ypred, f'/content/e0_val_cmat_before.png', normalize=True, title='before')
-        cmatrix(self.t_ygrnt, self.t_ypred, f'/content/e0_train_cmat_before.png', normalize=True, title='before')
+        cmatrix(self.v_ygrnt, self.v_ypred, f'/content/e0_val_cmat_before.png', normalize=True, title='after | Train')
+        cmatrix(self.t_ygrnt, self.t_ypred, f'/content/e0_train_cmat_before.png', normalize=True, title='after | Val')
         assert False, 'END-TRAINING'
 
-    def validation_step(self, batch, batch_idx, split='val'):
-        return
+    # def validation_step(self, batch, batch_idx, split='val'):
+    #     return
     def generator_step(self, batch, **kwargs):
         return super().generator_step__drcalgo(batch, **kwargs)
     
