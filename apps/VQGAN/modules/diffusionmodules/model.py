9885e7a9f1acb86eef405af7e5ae2ec476e4ef9b
alihejrati <alihejrati1996@gmail.com>
2023-11-18 17:12:18 +0330

    .

--- apps/VQGAN/modules/diffusionmodules/model.py
+++ apps/VQGAN/modules/diffusionmodules/model.py
@@ -575,8 +575,10 @@ class Decoder(nn.Module):
         self.start()
     
     def start(self):
-        self.spade_ilevel1 = SPADE(fch=128, xch=3, alphach=64, betach=128, gammach=128) # ([B, 128, 256, 256])
-        self.spade_endDownSampling = SPADE(fch=512, xch=3, alphach=64, betach=512, gammach=512, alphaconv_ksp='421', fwd='fwd2') # ([B, 512, 16, 16]) -> reshape: ([B, 2, 256, 256])
+        # self.spade_ilevel1 = SPADE(fch=128, xch=3, alphach=64, betach=128, gammach=128) # ([B, 128, 256, 256])
+        # self.spade_endDownSampling = SPADE(fch=512, xch=3, alphach=64, betach=512, gammach=512, alphaconv_ksp='421', fwd='fwd2') # ([B, 512, 16, 16]) -> reshape: ([B, 2, 256, 256])
+        self.spade_ilevel1 = lambda x, *ar: x
+        self.spade_endDownSampling = lambda x, *ar: x
     
     def forward(self, z, xc_lesion, h_ilevel1, h_endDownSampling):
         #assert z.shape[1:] == self.z_shape[1:]
