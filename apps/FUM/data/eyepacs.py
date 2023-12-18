5eeac4bdaa64c62865fac0d20c23907da21ef232
alihejrati <alihejrati1996@gmail.com>
2023-12-18 11:55:28 +0330

    .

--- apps/FUM/data/eyepacs.py
+++ apps/FUM/data/eyepacs.py
@@ -101,11 +101,11 @@ class D_DR(D_Base):
             # cpath = self.grade[cval][xc_idx]
             cpath = ospjoin('/content/RetinaLessions', fname, cval)
 
-            xc[cidx] = imgNormalizer(dr_transformer0(image=np.array(Image.open(ospjoin(cpath, 'fundus.jpg'))).astype(np.float32))['image'])
-            xcl[cidx] = imgNormalizer(dr_transformer0(image=np.array(Image.open(ospjoin(cpath, 'lesion.jpg'))).astype(np.float32))['image'])
-            xcc[cidx] = (dr_transformer_e(image=np.array(Image.open(ospjoin(cpath, 'cvh.jpg'))).astype(np.float32))['image']) / 255.0 # binary
-            xcf[cidx] = (dr_transformer0(image=np.array(Image.open(ospjoin(cpath, 'fmask.jpg'))).astype(np.float32))['image']) / 255.0 # single channell binary
-            xclmask[cidx] = (dr_transformer_e(image=np.array(Image.open(ospjoin(cpath, 'lmask.jpg'))).astype(np.float32))['image']) / 255.0 # binary
+            xc[cidx] = imgNormalizer(image=np.array(Image.open(ospjoin(cpath, 'fundus.jpg'))).astype(np.float32))['image'])
+            xcl[cidx] = imgNormalizer(image=np.array(Image.open(ospjoin(cpath, 'lesion.jpg'))).astype(np.float32))['image'])
+            xcc[cidx] = np.array(Image.open(ospjoin(cpath, 'cvh.jpg'))).astype(np.float32) / 255.0 # binary
+            xcf[cidx] = np.array(Image.open(ospjoin(cpath, 'fmask.jpg'))).astype(np.float32) / 255.0 # single channell binary
+            xclmask[cidx] = np.array(Image.open(ospjoin(cpath, 'lmask.jpg'))).astype(np.float32) / 255.0 # binary
             print(xc[cidx].shape)
             print(xcl[cidx].shape)
             print(xcc[cidx].shape)
