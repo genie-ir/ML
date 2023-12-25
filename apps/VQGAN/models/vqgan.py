45467d4bcea5f8e29b94db0dd8dcc8b800ab4ce5
alihejrati <alihejrati1996@gmail.com>
2023-12-25 15:57:11 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -617,15 +617,17 @@ class VQModel(pl.LightningModule):
     def synf(self, xscl0, m_c_union, xclmask, xcl):
         """xscl0 has information on `1 - m_union` and we want here, add information in `m_union area` to xscl0"""
         syn_xscl_input = m_c_union * xscl0 + xclmask * xcl
-        # signal_save(torch.cat([
-        #     (syn_xscl_input+1) * 127.5, 
-        #     (xscl0+1) * 127.5, 
-        #     (xcl+1) * 127.5, 
-        # ], dim=0), f'/content/export/syn.png', stype='img', sparams={'chw2hwc': True, 'nrow': 3})
-        # signal_save(torch.cat([
-        #     (m_c_union) * 255, 
-        #     (xclmask) * 255, 
-        # ], dim=0), f'/content/export/syn_m.png', stype='img', sparams={'chw2hwc': True, 'nrow': 3})
+        
+        signal_save(torch.cat([
+            (syn_xscl_input+1) * 127.5, 
+            (xscl0+1) * 127.5, 
+            (xcl+1) * 127.5, 
+        ], dim=0), f'/content/export/syn.png', stype='img', sparams={'chw2hwc': True, 'nrow': 3})
+        signal_save(torch.cat([
+            (m_c_union) * 255, 
+            (xclmask) * 255, 
+        ], dim=0), f'/content/export/syn_m.png', stype='img', sparams={'chw2hwc': True, 'nrow': 3})
+        
         assert False
         self.encoder.fwd_syn_step()
         return xscl0
