36953d0482f5b9885846d7c03138c238ffd38818
alihejrati <alihejrati1996@gmail.com>
2024-01-07 17:45:26 +0330

    .

--- apps/VQGAN/models/metrics.py
+++ apps/VQGAN/models/metrics.py
@@ -102,7 +102,7 @@ class Metrics(PYBASE):
     
     def reduction_accuracy(self, tag: str, mk: str, mv):
         globalname, localname = mk.split('/')
-        subname = '{}/{}'.format(globalname, localname.replace('ACC:reduction_accuracy', ''))
+        subname = '{}/{}'.format(globalname, localname.replace('ACC', ''))
         TP = sum(self.metrics[tag][f'{subname}TP:reduction_ignore'])
         TN = sum(self.metrics[tag][f'{subname}TN:reduction_ignore'])
         FP = sum(self.metrics[tag][f'{subname}FP:reduction_ignore'])
