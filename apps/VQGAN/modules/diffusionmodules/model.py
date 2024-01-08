6fac2e89b609aab6ce49af6c56383c9bce293e1a
alihejrati <alihejrati1996@gmail.com>
2024-01-08 22:42:14 +0330

    .

--- apps/VQGAN/modules/diffusionmodules/model.py
+++ apps/VQGAN/modules/diffusionmodules/model.py
@@ -373,6 +373,7 @@ class Encoder(nn.Module):
         ################################################################################# for VQGAN
         self.Qsurface2Qdiagonal = torch.nn.Conv2d(256, 256, 3, 1, 1)
         self.netb_diagonal = nn.Sequential(
+            View(),
             Reshape256To16x16(),
             nn.ConvTranspose2d(1, 16, 4,2,1), #32x32
             nn.Tanh(),
@@ -386,7 +387,7 @@ class Encoder(nn.Module):
             nn.Tanh(),
             nn.Conv2d(64, 1, 3,2,1), #64x64
             Reshape64x64ToV16x256(),
-            View()
+            
             # nn.Linear(256, 1024),
             # nn.Tanh(),
             # nn.Linear(1024, 16*256),
