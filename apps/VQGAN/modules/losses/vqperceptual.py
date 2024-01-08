03734c66a35047d1b619db7facde935fa3670ddd
alihejrati <alihejrati1996@gmail.com>
2024-01-08 21:09:19 +0330

    .

--- apps/VQGAN/modules/losses/vqperceptual.py
+++ apps/VQGAN/modules/losses/vqperceptual.py
@@ -83,9 +83,9 @@ class VQLPIPSWithDiscriminator(nn.Module):
         self.vgg16.classifier[6] = nn.Sequential(
             Reshape4096To64x64(),
             nn.Conv2d(1, 32, 3, 2, 1), #32x32
-            nn.Conv2d(1, 64, 3, 2, 1), #16x16
-            nn.Conv2d(1, 128, 3, 2, 1), #8x8
-            nn.Conv2d(1, 256, 3, 2, 1), #4x4
+            nn.Conv2d(32, 64, 3, 2, 1), #16x16
+            nn.Conv2d(64, 128, 3, 2, 1), #8x8
+            nn.Conv2d(128, 256, 3, 2, 1), #4x4
             View(),
             # nn.Conv2d(1, 256, 4, 1, 0), #4x4
             # nn.Linear(n_inputs, 256)
