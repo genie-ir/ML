c88b1b4b559ab25788d50cd083a7a294108177f7
alihejrati <alihejrati1996@gmail.com>
2024-01-08 22:16:41 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -322,9 +322,6 @@ class VQModel(pl.LightningModule):
         Qsurface = (1-q_eye16) * Qorg
         Qdiagonal = self.encoder.Qsurface2Qdiagonal(Qsurface.detach())
 
-        print('Qsurface', Qsurface.shape)
-        print('Qdiagonal', Qdiagonal.shape)
-        assert False
         
         return h_ilevel1, h_endDownSampling, q_eye16, Qsurface, Qorg, Qdiagonal
         
