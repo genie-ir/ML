0ad7ef2baed75728e1a0246ecb194da7d011bc40
alihejrati <alihejrati1996@gmail.com>
2024-01-09 18:10:32 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -573,7 +573,7 @@ class VQModel(pl.LightningModule):
             # M_L_xs_mines_xc = (xslmask - (xslmask * xclmask)).detach() # TODO Interpolation
             M_C_Union = ((1 - M_union_L_xs_xc) * xsf).detach() #shape:torch.Size([1, 1, 256, 256]) # reconstruct xs
             # M_xrec_xcl = (xclmask * xsf).detach() # reconstruct xc
-            xcm_gray = (xclmask * xc).mean(dim=1, keepdim=True).detach() # torch.Size([1, 1, 256, 256])
+            xcm_gray = (xclmask * xc * xsf).mean(dim=1, keepdim=True).detach() # torch.Size([1, 1, 256, 256])
             C_xcmask = (1-xclmask).detach()
 
             signal_save(torch.cat([
@@ -587,7 +587,7 @@ class VQModel(pl.LightningModule):
             #         signal_save(torch.cat([
             #             (xs+1)*127.5, (xsl+1)*127.5, self.cat3d(xsf)*255, self.cat3d(xslmask)*255, 
             #             (xc+1)*127.5, (xcl+1)*127.5, self.cat3d(xcf)*255, self.cat3d(xclmask)*255, 
-            #             ((xs*C_xsmask)+1)*127.5, xsf * (self.cat3d(xcm_gray)+1)*127.5, xsf * ((xs*M_C_Union + xc*xclmask)+1)*127.5, xsf * self.cat3d(M_union_L_xs_xc)*255, 
+            #             ((xs*C_xsmask)+1)*127.5, (self.cat3d(xcm_gray)+1)*127.5, xsf * ((xs*M_C_Union + xc*xclmask)+1)*127.5, xsf * self.cat3d(M_union_L_xs_xc)*255, 
             #         ], dim=0), f'/content/export/dataset/B{batch_idx}.png', stype='img', sparams={'chw2hwc': True, 'nrow': 4})
 
 
