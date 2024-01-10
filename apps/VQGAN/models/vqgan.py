35feb11c498b9a9457b00b2e8442e74a8e68af60
alihejrati <alihejrati1996@gmail.com>
2024-01-10 18:48:20 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -287,8 +287,8 @@ class VQModel(pl.LightningModule):
         # self.decoder_grad_controller(True)
         h_ilevel1, h_endDownSampling, q_eye16, Qsurface, Qorg, Qdiagonal = self.net(simg)
         y = self.decoder(
-            # Qorg,
-            (q_eye16) * Qorg,
+            Qorg,
+            # (q_eye16) * Qorg,
             # (1 - q_eye16) * Qorg,
             None, 
             h_ilevel1, 
