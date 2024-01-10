66a55655b50a21c3fc8ea011ca2545e9931cc526
alihejrati <alihejrati1996@gmail.com>
2024-01-10 14:23:15 +0330

    .

--- apps/VQGAN/modules/diffusionmodules/model.py
+++ apps/VQGAN/modules/diffusionmodules/model.py
@@ -447,8 +447,6 @@ class ConvT_Tanh_SN(nn.Module):
         self.z3 = ConvT_Tanh(32, 16, 4,2,1)#16x16
 
     def forward(self, x): # x is surface 1x256x16x16
-        print('x', x[0,:,7,8])
-        assert False
         # q_eye16 = torch.eye(16, dtype=torch.float32, device='cuda').detach()
         z = torch.randn((1,256,1,1), device='cuda')
         c0 = self.c0(x) + self.C0(x)
