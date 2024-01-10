6a675fd0a7d990d180d166b1f230f5b571df63e8
alihejrati <alihejrati1996@gmail.com>
2024-01-10 18:15:25 +0330

    .

--- apps/VQGAN/modules/diffusionmodules/model.py
+++ apps/VQGAN/modules/diffusionmodules/model.py
@@ -468,12 +468,13 @@ class ConvT_Tanh_SN(nn.Module):
         zout = z3.view(256, 16, 1, 1)
         zeros = torch.zeros(256, 16, 16, dtype=torch.float32, device='cuda').detach()
         V = (zout + zeros.unsqueeze(-1)).view((1, 256, 16, 16))
-        V.register_hook(lambda grad: wg * grad)
+        
         # print('x', x.min().item(), x.max().item(), x.mean().item(), x)
         # print('V', V.min().item(), V.max().item(), V.mean().item(), V)
         # print('x-V', x-V)
         # V.register_hook(self.Vgrad)
         if flag:
+            V.register_hook(lambda grad: wg * grad)
             V.register_hook(lambda grad: print(wg, 'V.grad', grad.mean()))
         # V.register_hook(lambda grad: print('V.grad', grad))
         
