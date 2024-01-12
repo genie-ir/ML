671fd2306fd0fb587160a16da22b0ddfa1a198de
alihejrati <alihejrati1996@gmail.com>
2024-01-12 18:05:32 +0330

    .

--- apps/VQGAN/models/vqgan.py
+++ apps/VQGAN/models/vqgan.py
@@ -412,8 +412,8 @@ class VQModel(PLModule):
         for param in self.encoder.kernel_regressor.parameters():
             param.requires_grad = True
         
-        # for param in self.quantize.parameters():
-        #     param.requires_grad = True
+        for param in self.quantize.parameters():
+            param.requires_grad = True
         
         for pidx in [5, 6, 8, 9, 11]:
             for param in self.Loss.discriminator.main[pidx].parameters():
