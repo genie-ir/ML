62cbaeac95163dd5288a43cb89c6d883a92dd7f5
alihejrati <alihejrati1996@gmail.com>
2024-01-19 00:46:37 +0330

    .

--- apps/VQGAN/models/learners.py
+++ apps/VQGAN/models/learners.py
@@ -212,7 +212,8 @@ class BST_Regressor(Lerner):
         self.__start()
 
     def __start(self):
-        self.BST = self.List([BST(**self.kwargs) for b in range(self.β)])
+        # self.BST = self.List([BST(**self.kwargs) for b in range(self.β)])
+        self.bst = BST(**self.kwargs)
 
     def forward(self, bipolar):
         """single bipolar current signal enters and the output is single bipolar signal"""
@@ -292,124 +293,12 @@ class FUM_H_Graph(Lerner):
         super().__init__(**kwargs)
         self.a = Node(inch=256, outch=256, k=3, s=1, p=1)
         self.b = Node(inch=256, outch=256, k=3, s=1, p=1) 
-        self.c = Node(inch=512, outch=256, k=3, s=1, p=1)
+        self.c = Node(inch=256, outch=256, k=3, s=1, p=1)
     
     def forward(self, x1, x2):
         y1 = self.a(x1)
         y2 = self.b(x2)
-        y = self.c(torch.cat([y1, y2], dim=1))
+        y = self.c((y1 + y2) / 2)
         return y
     
 
-
-
-    
-
-class T(Lerner):
-    def __init__(self, T='conv', us=False, ds=True, b=1, n=100, act='tanh', actparams=None, **kwargs):
-        super().__init__(**kwargs)
-
-        self.us = bool(us) # up sampling
-        self.ds = bool(ds) # down sampling
-        assert (self.ds == True and self.us == False) or (self.ds == False and self.us == True)
-
-        self.b = int(b) # branching factor
-        self.n = int(n) # sinusoidal factor
-
-        if T.lower() == 'la':
-            T = 'LA'
-            pass # TODO
-        
-        if 'conv' in T.lower():
-            inch = self.kwargs.get('inch', None)
-            outch = self.kwargs.get('outch', None)
-
-            if self.ds:
-                k = self.kwargs.get('k', 3)
-                s = self.kwargs.get('s', 2)
-                p = self.kwargs.get('p', 1)
-            else:
-                k = self.kwargs.get('k', 4)
-                s = self.kwargs.get('s', 2)
-                p = self.kwargs.get('p', 1)
-
-            if T.lower() == 'conv2d' or T.lower() == 'conv': # 2D
-                if self.ds:
-                    T = 'Conv2d'
-                else:
-                    T = 'ConvTranspose2d'
-            
-            if inch != None:
-                self.kwargs['in_channels'] = int(inch)
-            
-            if outch != None:
-                self.kwargs['out_channels'] = int(outch)
-            
-            if k != None:
-                self.kwargs['kernel_size'] = int(k)
-            
-            if s != None:
-                self.kwargs['stride'] = int(s)
-            
-            if p != None:
-                self.kwargs['padding'] = int(p)
-
-        self.T = T
-
-        self.act = act
-        self.actparams = actparams if actparams != None else dict()
-
-        self.__start()
-
-    def __start(self):
-        fwd = '_fwd'
-        if self.act != None:
-            fwd = '_fwd_act'
-            # self.component(self.Activation, 'afn1', T=self.act, **self.actparams)
-            self.component(self.Activation, 'afn1', T='tanh', **self.actparams)
-            self.component(self.Activation, 'afn2', T='sig', **self.actparams)
-        
-        setattr(self, 'fwd', getattr(self, fwd))
-        
-        self.l = self.List([self.component(self.T, **self.kwargs) for b in range(self.b)])
-
-        bfwd = 'fwd'
-        if self.b > 1:
-            bfwd = '_bfwd'
-            self.merge_params()
-        
-        setattr(self, 'bfwd', getattr(self, bfwd))
-
-
-    def merge(self, l):
-        """overwitable method"""
-        return self._merge(torch.cat(l, dim=1))
-    
-    def merge_params(self):
-        """overwitable method"""
-        self._merge = self.component(
-            'Conv2d', 
-            in_channels = self.b * self.kwargs['out_channels'],
-            out_channels = self.kwargs['out_channels'],
-            kernel_size=1,
-            stride=1,
-            padding=0
-        )
-
-    def _fwd(self, x, i=0):
-        y = self.l[i](x)
-        # TODO control derivative
-        return y
-    
-    def _fwd_act(self, x, i=0):
-        return self.activation(self._fwd(x, i))
-    
-    def _bfwd(self, x):
-        l = []
-        for b in range(self.b):
-            l.append(self.fwd(x, b))
-        return self.merge(l)
-    
-    def forward(self, x):
-        y = self.bfwd(x)
-        # sinsoide!!!!!!
